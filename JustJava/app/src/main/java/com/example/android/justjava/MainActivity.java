b89c6ee16d7ffbdb3cd1a8a1ca1dcfb8fa42d410
sbj1811 <sbj1811@github.com>
2018-02-09 02:05:00 -0800

    JustJava V1.0

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
     boolean checkBox1_status;
+    boolean checkBox2_status;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,26 +29,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void itemClicked(View view){
+    /**
+     * Catures if checkbox 1 is clicked
+     *
+     * @param view checkbox
+     */
+
+    public void itemClicked1(View view) {
         CheckBox checkBox1 = (CheckBox) view;
         checkBox1_status = checkBox1.isChecked();
     }
 
     /**
-     * This method is called when the order button is clicked.
+     * Catures if checkbox 2 is clicked
+     *
+     * @param view checkbox
      */
-    public void submitOrder(View view) {
-        int price = calculatePrice(quantity);
-        displayMessage(createOrderSummary(price));
-    }
-
-    private String createOrderSummary(int price){
-        String priceMessage = "Name: Shaunak Jani"+"\nAdd Whipped cream? "+checkBox1_status+"\nQuantity: "+quantity+"\nTotal: $"+ price + "\nThank you!";
-        return priceMessage;
+    public void itemClicked2(View view) {
+        CheckBox checkBox2 = (CheckBox) view;
+        checkBox2_status = checkBox2.isChecked();
     }
 
     /**
-     * This method displays the given quantity value on the screen.
+     * Order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int price = calculatePrice(quantity);
+        EditText nameEditText = (EditText) findViewById(R.id.name_field);
+        name = nameEditText.getText().toString();
+        String order = createOrderSummary(name, price);
+        String subject = "JustJava order for " + name;
+        composeEmail(subject,order);
+    }
+
+    /**
+     * Creates Email intent to open Email app
+     * @param subject of the email
+     * @param body which contains order summary
+     */
+
+    public void composeEmail(String subject,String body) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * Creates Order Summary when Order button is Clicked
+     *
+     * @param name  of customer
+     * @param price of the order
+     */
+    private String createOrderSummary(String name, int price) {
+        String priceMessage = "Name: " + name;
+        priceMessage += "\nAdd Whipped cream? " + checkBox1_status;
+        priceMessage += "\nAdd Chocolate? " + checkBox2_status;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
+    }
+
+
+
+    /**
+     * Displays the given quantity value on the screen.
      */
     private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
@@ -52,11 +107,18 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Calculates the price of the order based on the current quantity.
      *
+     * @param quantity selected
      * @return the price
      */
     private int calculatePrice(int quantity) {
-        int price = quantity * 5;
-        return price;
+        int basePrice = 5;
+        if (checkBox1_status){
+            basePrice+=1;
+        }
+        if (checkBox2_status){
+            basePrice+=2;
+        }
+        return quantity * basePrice;
     }
 
     /**
@@ -72,6 +134,10 @@ public class MainActivity extends AppCompatActivity {
      * This method increments the quantity.
      */
     public void increment(View view) {
+        if (quantity==100){
+            Toast.makeText(this, "You cannot have more than 100 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity++;
         displayQuantity(quantity);
     }
@@ -80,6 +146,10 @@ public class MainActivity extends AppCompatActivity {
      * This method decrements the quantity.
      */
     public void decrement(View view) {
+        if (quantity<1){
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity--;
         displayQuantity(quantity);
     }
